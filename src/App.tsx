b026816673e1fc7fb2d4eb136bbe4df6e8677862
Yura <bobr515yura@yandex.ru>
2025-03-29 11:36:27 +0300

    HW

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,56 @@
-import Card from './components/Card/Card';
-import data from './data/data.json'; 
-import cardImage from './assets/card-image.png'; 
+import { useState } from 'react';
+import WatchForm from './components/WatchForm/WatchForm';
+import WatchList from './components/WatchList/WatchList';
+import IWatch from './models/IWatch';
 
 const App = () => {
+  const [form, setForm] = useState<IWatch>({ zone: '', offset: '' }); // данные из формы
+  const [watches, setWatches] = useState<IWatch[]>([]); // все установленные часы
+  const [tooltip, setTooltipText] = useState({zoneTooltip: '', offsetTooltip: ''}); // подсказка
+
+  const handleChange = (newForm: IWatch) => {
+    setForm(newForm); // актуализация полей формы
+    setTooltipText({zoneTooltip: '', offsetTooltip: ''}); // убираем подсказки
+  };
+
+  const handleSubmit = (newForm: IWatch) => {
+    if (newForm.zone === '') {
+      setForm(newForm); // сброс невалидного инпута с городом
+      setTooltipText({zoneTooltip: 'А это точно город?', offsetTooltip: ''}); // подсказка
+      return;
+    }
+
+    if (newForm.offset === '') {
+      setForm(newForm); // сброс невалидного инпута с часовым сдвигом
+      setTooltipText({zoneTooltip: '', offsetTooltip: 'Введите число от 0 до 23'}); // подсказка
+      return;
+    }
+
+    // если данные полей ввода валидны и полные:
+    const exists = watches.find((el) => el.zone === newForm.zone);
+    // если город уже есть:
+    if (exists) {
+      setForm({ zone: '', offset: '' }); // очистка полей формы
+      setTooltipText({zoneTooltip: 'Город уже был добавлен', offsetTooltip: ''}); // подсказка
+      return;
+    }
+    setWatches([...watches, newForm]); // актуализация массива с данными
+    setForm({ zone: '', offset: '' }); // очистка полей формы
+  };
+
+  const handleRemove = (watch: IWatch) => {
+    setWatches(watches.filter((el) => el.zone !== watch.zone)); // удаляем часы
+  };
+
   return (
     <>
-      {/* NOTE: Вариант без chidren: */}
-      {data.map((item) => (
-        <Card key={item.id} item={item} />
-      ))}
-
-      {/* NOTE: Вариант с chidren: */}
-      <Card item={{...data[0], title: "Card title 8", text: "This is a card with children!!!"}}>
-        <img className="card__img" src={cardImage} alt="card not found" />
-      </Card>
+      <WatchForm
+        form={form}
+        tooltip={tooltip}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      {watches.length ? <WatchList watches={watches} onRemove={handleRemove} /> : null}
     </>
   );
 };
